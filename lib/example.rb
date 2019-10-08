5a6634c242353bac806b82572e4e642edc0a58d8
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-08 19:04:55 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -3,11 +3,12 @@ def say_hello_world_ten_times
   phrase = "Hello World!"
   puts phrase
   puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
